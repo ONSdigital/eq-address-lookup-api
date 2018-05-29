f4ee0da59fad8fab96c9e9aec5e3c7f77d39566c
dwyeradam <adam.dwyer@ons.gov.uk>
2018-05-29 15:21:56 +0100

    Add timeout

--- app/request_address.py
+++ app/request_address.py
@@ -4,6 +4,7 @@ import requests
 
 address_lookup_url = 'http://addressindex-api-dev.apps.devtest.onsclofo.uk/addresses'
 result_limit = '100'
+request_timeout = 30
 
 def query_address_index(search_value):
     '''
@@ -15,13 +16,18 @@ def query_address_index(search_value):
                 r'|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])'
                 r'|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z]))))\s?[0-9][A-Za-z]{2})', search_value):
         # Postcode search
-        resp = requests.get(address_lookup_url + '/postcode/' + search_value)
+        request_string = address_lookup_url + '/postcode/' + search_value
     else:
-        resp = requests.get(address_lookup_url + '?input=' + search_value + ';limit=' + result_limit)
+        request_string = address_lookup_url + '?input=' + search_value + ';limit=' + result_limit
+
+    try:
+        resp = requests.get(request_string, timeout=request_timeout)
+    except (requests.exceptions.ReadTimeout):
+        return ["ADDRESS INDEX API REQUEST TIMED OUT"]
 
     if resp.status_code != 200:
         # This means something went wrong.
-        return ["ERROR IN ADDRESS INDEX API (try refresh)"]
+        return ["ERROR IN ADDRESS INDEX API (try refresh). Reason: " + resp.reason]
 
     return get_addresses(resp)
 
