c3f41e12dc4abbb426084bd36bcb75e08d64b415
Phil Bambridge <phil.bambridge@ons.gov.uk>
2019-04-17 09:41:21 +0100

    Add k8s/helm scripts for deployment to GCP

---
+++ k8s/deploy_app.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [[ -z "$1" ]]; then
+  echo "LOOKUP_URL is mandatory"
+  exit 1
+fi
+
+LOOKUP_URL=$1
+DOCKER_REGISTRY="${2:-eu.gcr.io/census-eq-ci}"
+IMAGE_TAG="${3:-latest}"
+
+helm tiller run \
+    helm upgrade --install \
+    address-lookup-api \
+    k8s/helm \
+    --set lookup_url=${LOOKUP_URL} \
+    --set image.repository=${DOCKER_REGISTRY}/eq-address-lookup-api \
+    --set image.tag=${IMAGE_TAG}
