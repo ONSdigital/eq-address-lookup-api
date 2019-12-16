6c1964e51a5a3161ad90a6eab9ad5110323b2c06
pricem14pc <mark.price@ons.gov.uk>
2019-12-16 17:16:34 +0000

    Adding uprn and response object change

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ from flask_cors import CORS
 from structlog import get_logger
 
 from app.request_address import query_address_index
+from app.request_address import query_address_index_by_uprn
 
 logger = get_logger()
 
@@ -24,7 +25,7 @@ def before_request():
 def after_request(response):
     if g.get('request_start_time') and g.get('address_index_response_time'):
         lookup_api_response_time = time.time() - g.request_start_time
-        logger.info("Request Complete",
+        logger.info('Request Complete',
                     lookup_api_response_time=lookup_api_response_time,
                     lookup_api_processing_time=lookup_api_response_time - g.address_index_response_time)
     return response
@@ -43,23 +44,52 @@ def status():
     return json.dumps(data)
 
 
-@app.route('/address_api/', methods=['GET'])
+@app.route('/address_api/', methods=['GET', 'POST'])
 def address_search():
-    query = request.args.get('q')
+
+    if request.method == 'POST':
+        query = request.form['q']
+    else:
+        query = request.args.get('q')
+
     if query:
         try:
-            addresses = query_address_index(query)
-            return dict(addresses=addresses, count=len(addresses))
+            resp = query_address_index(query)
+            return dict(addresses=resp['addresses'], count=len(resp['addresses']), time=resp['time'])
         except requests.exceptions.ConnectionError:
             return 'ADDRESS INDEX API CONNECTION ERROR', 503
         except requests.exceptions.ConnectTimeout:
-            return "ADDRESS INDEX API CONNECTION TIMED OUT", 503
+            return 'ADDRESS INDEX API CONNECTION TIMED OUT', 503
         except requests.exceptions.ReadTimeout:
-            return "ADDRESS INDEX API READ TIMED OUT", 408
+            return 'ADDRESS INDEX API READ TIMED OUT', 408
         except Exception as exception:
             return str(exception), 500
     else:
-        return "Enter address_api/?q=Address"
+        return 'Enter address_api/?q=Address'
+
+@app.route('/uprn/', methods=['GET', 'POST'])
+def address_search_by_uprn():
+
+    if request.method == 'POST':
+        query = request.form['q']
+    else:
+        query = request.args.get('q')
+
+    if query:
+        try:
+            resp = query_address_index_by_uprn(query)
+            return dict(address=resp['address'], count=len(resp['address']), time=resp['time'])
+        except requests.exceptions.ConnectionError:
+            return 'ADDRESS INDEX API CONNECTION ERROR', 503
+        except requests.exceptions.ConnectTimeout:
+            return 'ADDRESS INDEX API CONNECTION TIMED OUT', 503
+        except requests.exceptions.ReadTimeout:
+            return 'ADDRESS INDEX API READ TIMED OUT', 408
+        except Exception as exception:
+            return str(exception), 500
+    else:
+        return 'Enter uprn/?q=uprn'
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
--- app/request_address.py
+++ app/request_address.py
@@ -12,44 +12,106 @@ logger = get_logger()
 
 
 def query_address_index(search_value):
-    '''
+    """
     Postcode definition Regex taken from:
     https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/488478/
         Bulk_Data_Transfer_-_additional_validation_valid_from_12_November_2015.pdf
-    '''
+    """
     if re.match(r'([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})'
                 r'|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])'
-                r'|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z]))))\s?[0-9][A-Za-z]{2})', search_value):
+                r'|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z]))))\s?[0-9][A-Za-z]{2})',
+                search_value):
         # Postcode search
-        request_string = "/".join((settings.LOOKUP_URL, 'addresses', 'postcode', search_value))
+        request_string = f'{settings.LOOKUP_URL}/addresses/postcode/{search_value}'
     else:
-        request_string = "/".join(
-            (settings.LOOKUP_URL, 'addresses', 'partial', search_value + '?limit=' + settings.RESULT_LIMIT))
+        request_string = f'{settings.LOOKUP_URL}/addresses/partial'\
+            f'?input={search_value}'\
+            f'&limit={settings.RESULT_LIMIT}'
 
-    headers = {
-        'Authorization': settings.AUTH_KEY
-    }
-
-    logger.info("Request data from address index", url=request_string)
+    logger.info('Request data from address index', url=request_string)
 
     address_index_request_start = time.time()
+    headers = {'content-type': 'application/json'}
     resp = requests.get(request_string,
                         timeout=(settings.REQUEST_CONNECTION_TIMEOUT,
                                  settings.REQUEST_READ_TIMEOUT),
+                        auth=(settings.AUTH_KEY, ''),
                         headers=headers)
     g.address_index_response_time = time.time() - address_index_request_start
     logger.bind(address_index_response_time=g.address_index_response_time)
 
     if resp.status_code != 200:
         # This means something went wrong.
-        raise Exception("ERROR IN ADDRESS INDEX API (try refresh). Reason: {}. Response: {}".format(resp.reason, resp.text))
+        raise Exception('ERROR IN ADDRESS INDEX API (try refresh).'
+                        f' Reason: {resp.reason}. Response: {resp.text}')
 
-    return get_addresses(resp)
+    return dict(addresses=build_addresses(resp), time=g.address_index_response_time)
+
+
+def query_address_index_by_uprn(search_value):
+
+    request_string = f'{settings.LOOKUP_URL}/addresses/uprn/{search_value}?verbose=true'
+
+    logger.info('UPRN Request data from address index', url=request_string)
+
+    address_index_request_start = time.time()
+    headers = {'content-type': 'application/json'}
+    resp = requests.get(request_string,
+                        timeout=(settings.REQUEST_CONNECTION_TIMEOUT,
+                                 settings.REQUEST_READ_TIMEOUT),
+                        auth=(settings.AUTH_KEY, ''),
+                        headers=headers)
+    g.address_index_response_time = time.time() - address_index_request_start
+    logger.bind(address_index_response_time=g.address_index_response_time)
+
+    if resp.status_code != 200:
+        # This means something went wrong.
+        raise Exception('ERROR IN ADDRESS INDEX API (try refresh).'
+                        f' Reason: {resp.reason}. Response: {resp.text}')
+
+    return dict(address=build_address(resp), time=g.address_index_response_time)
+
 
 def get_addresses(resp):
     addresses = []
     result = json.loads(resp.text)
+    logger.info('Address Index API response data', data=result)
     for address in result['response']['addresses']:
         addresses.append(address['formattedAddress'])
+    logger.info('eQ Address Lookup API response data', data=addresses)
+    return addresses
 
+
+def build_address(resp):
+    result = json.loads(resp.text)
+    logger.info('UPRN Building address from Address Index response data', data=result)
+    address_response = result['response']['address']
+    address_fields = address_response['formattedAddress'].split(", ")
+    i = 0
+    address = {}
+    for value in address_fields:
+        i = i + 1
+        key = 'field' + str(i)
+        address[key] = value
+    address["uprn"] = address_response['uprn']
+    address["formattedAddress"] = address_response['formattedAddress']
+    address["welshFormattedAddressNag"] = address_response['welshFormattedAddressNag']
+    # There will be 0 or 1 paf
+    address["paf"] = address_response['paf']
+    # There will be >= 1 nag
+    address["nag"] = address_response['paf']
+    logger.info('UPRN Formatted data from eQ API', data=address)
+    return address
+
+
+def build_addresses(resp):
+    addresses = []
+    result = json.loads(resp.text)
+    logger.info('Building addresses from Address Index response data', data=result)
+    for address_line in result['response']['addresses']:
+        address = {}
+        address["uprn"] = address_line['uprn']
+        address["text"] = address_line['formattedAddress']
+        addresses.append(address)
+    logger.info('Formatted data from eQ API', data=addresses)
     return addresses
